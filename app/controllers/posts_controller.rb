3dd3943c2d9d22f185ed256f51d6d1b15af78a1f
Takaho Takemura <jp.takapon@gmail.com>
2023-08-24 00:50:13 +0900

    Add post UI

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -39,13 +39,14 @@ class PostsController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def post_params
-      params.require(:post).permit(:title, :body)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  # Only allow a list of trusted parameters through.
+  def post_params
+    params.require(:post).permit(:id, :title, :body)
+  end
 end
---
+++ client/src/components/Posts.vue
@@ -0,0 +1,102 @@
+<template>
+  <div class="container">
+        <h1>Post your note</h1>
+        <form class="crud-form">
+            <label for="title">Title:</label>
+            <input
+              v-model="enteredTitle"
+              class="input-field"
+              name="title"
+              placeholder="Enter title"
+              type="text"
+            >
+
+            <label for="content">Content:</label>
+            <textarea
+              v-model="enteredContent"
+              class="input-field"
+              name="content"
+              placeholder="Enter content"
+            ></textarea>
+
+            <button type="submit" class="submit-button">Save</button>
+          </form>
+    </div>
+</template>
+
+<script setup>
+//////////////////////////////
+// imports
+import { reactive, ref } from 'vue';
+
+const enteredTitle = ref("")
+const enteredContent = ref("")
+
+const notes = reactive({
+  id: new Date().valueOf(),
+  title: "Hello",
+  content: "Hello, this is the very first post"
+
+})
+</script>
+
+<style scoped>
+body {
+    font-family: Arial, sans-serif;
+    background-color: #f4f4f4;
+    margin: 0;
+    padding: 0;
+}
+
+.container {
+    max-width: 600px;
+    margin: 0 auto;
+    padding: 20px;
+    background-color: #ffffff;
+    box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+    border-radius: 5px;
+}
+
+h1 {
+    text-align: center;
+    margin-bottom: 20px;
+}
+
+.crud-form {
+    display: grid;
+    gap: 10px;
+}
+
+label {
+    font-weight: bold;
+}
+
+.input-field {
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    font-size: 16px;
+}
+
+.textarea-field {
+    padding: 10px;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    font-size: 16px;
+}
+
+.submit-button {
+    background-color: #007bff;
+    color: #ffffff;
+    border: none;
+    border-radius: 5px;
+    padding: 10px 20px;
+    font-size: 16px;
+    cursor: pointer;
+}
+
+.submit-button:hover {
+    background-color: #0056b3;
+}
+
+</style>
